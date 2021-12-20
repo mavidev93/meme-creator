cdfcd611c4197976ecafa523e8327b0863a43be1
a.aghasi <yakamoz92a@gmail.com>
2021-12-20 12:18:18 +0330

    initial commit- node-sass added

---
+++ .history/src/components/App_20211220121710.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Counter } from "../features/counter/Counter";
+import "./App.scss";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
